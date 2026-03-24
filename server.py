857fa9446525ee82b7a6ad57d2339ec67254817b
jeki_dev <jekidev@proton.me>
2026-03-24 02:58:54 +0100

    Default dashboard port 5050

--- server.py
+++ server.py
@@ -61,5 +61,5 @@ if __name__ == "__main__":
     print("Starting Valkyrie Cloud")
     manager.start_all()
 
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5050))
     app.run(host="0.0.0.0", port=port)
